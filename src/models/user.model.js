e2a2930764f5202219f077d3cd13d8dc11337244
Ksona <ksona@gmail.com>
2026-02-23 16:12:51 +0530

    added user schema and hashed the password

---
+++ src/models/user.model.js
@@ -0,0 +1,78 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        profileImage: {
+            type: {
+                url: String,
+                localPath: String,
+            },
+            default: {
+                url: 'https://placehold.co/200x200',
+                localPath: String,
+            },
+        },
+        fullName: {
+            type: String,
+            required: true,
+            min: 3,
+            max: [50, 'Name should be less than 50 characters'],
+        },
+        userName: {
+            type: String,
+            required: true,
+            min: 3,
+            max: [50, 'Name should be less than 50 characters'],
+            index: true,
+            lowerCase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            lowerCase: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'password is required'],
+            trim: true,
+            min: 4,
+            max: 30,
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        refreshToken: {
+            type: String,
+        },
+        emailVerificationToken: {
+            type: String,
+        },
+        emailVerificationTokenExpiry: {
+            type: Date,
+        },
+        forgotPasswordToken: {
+            type: String,
+        },
+        forgotPasswordTokenExpiry: {
+            type: Date,
+        },
+    },
+    { timestamps: true }
+);
+
+// hash the password
+userSchema.pre("save",async function () {
+   if (!this.isModified('password')) {
+     return 
+   }
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password,salt);
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
